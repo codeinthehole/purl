d530c36085b2a281f874cbafbf5e60048b65f598
David Winterbottom <david.winterbottom@gmail.com>
2012-04-22 21:43:17 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup, find_packages
 
 setup(name='purl',
-      version='0.3',
+      version='0.3.1',
       url='https://github.com/codeinthehole/purl',
       author="David Winterbottom",
       author_email="david.winterbottom@gmail.com",
-      description="A simple URL class for easy URL-building and manipulation",
+      description="An immutable URL class for easy URL-building and manipulation",
       long_description=open('README.rst').read(),
       packages=find_packages(exclude=['tests']),
       )
