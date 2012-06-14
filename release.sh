4d1fa23bb89615a57903d48bfdc82a37acdbd6da
David Winterbottom <david.winterbottom@gmail.com>
2012-06-14 22:28:10 +0100

    Updated release script for git-flow

--- release.sh
+++ release.sh
@@ -5,15 +5,9 @@
 git diff --quiet HEAD
 [ $? -ne 0 ] && echo "Uncommitted changes!" && exit 1
 
-# Pluck release number out of setup.py
-RELEASE_NUM=`grep version setup.py | cut -d\' -f2`
-git tag | grep $RELEASE_NUM > /dev/null && \
-	echo "New version number required ($RELEASE_NUM already used)" && exit 1
-
 # Push to PyPi
 ./setup.py sdist upload
 
 # Tag in Git
-git tag $RELEASE_NUM -m "Tagging release $RELEASE_NUM"
 git push origin master
 git push --tags
