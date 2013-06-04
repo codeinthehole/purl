c3e1144c9ea43a43db1c5991e7d6fff7d5b79c4f
David Winterbottom <david.winterbottom@gmail.com>
2013-06-04 21:54:04 +0100

    3.2.4 - Fragment expansion with value modifiers

--- tests/uritemplate_tests.py
+++ tests/uritemplate_tests.py
@@ -71,6 +71,12 @@ data = [
     ('{+list*}', level4_vars, 'red,green,blue'),
     ('{+keys}', level4_vars, 'semi,;,dot,.,comma,,'),
     ('{+keys*}', level4_vars, 'semi=;,dot=.,comma=,'),
+    # Level 4 - fragment expansion with value modifiers
+    ('{#path:6}/here', level4_vars, '#/foo/b/here'),
+    ('{#list}', level4_vars, '#red,green,blue'),
+    ('{#list*}', level4_vars, '#red,green,blue'),
+    ('{#keys}', level4_vars, '#semi,;,dot,.,comma,,'),
+    ('{#keys*}', level4_vars, '#semi=;,dot=.,comma=,'),
 ]
 _data = [
     ('{list}', level4_vars, 'red,green,blue'),
