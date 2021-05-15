2bd51cabecfd4dcd20544fba7092cfd98dc7dac0
David Winterbottom <david.winterbottom@gmail.com>
2021-05-15 17:36:29 +0100

    Bump version to v1.6

--- purl/__init__.py
+++ purl/__init__.py
@@ -1,6 +1,6 @@
 from .url import URL  # noqa
 from .template import expand, Template  # noqa
 
-__version__ = '1.5'
+__version__ = '1.6'
 
 __all__ = ['URL', 'expand', 'Template']
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='purl',
-    version='1.5',
+    version='1.6',
     description=(
         "An immutable URL class for easy URL-building and manipulation"),
     long_description=open('README.rst').read(),
