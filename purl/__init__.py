0a676e8f21c4039ee39f4f4cec73c7e38670b644
Christian Clauss <cclauss@me.com>
2022-05-03 15:24:44 +0200

    Revert

--- purl/__init__.py
+++ purl/__init__.py
@@ -1,7 +1,5 @@
-from __future__ import absolute_import
-
-from purl.url import URL  # noqa
-from purl.template import expand, Template  # noqa
+from .url import URL  # noqa
+from .template import expand, Template  # noqa
 
 __version__ = '1.6'
 
