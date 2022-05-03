cd3b724654a15503833c5159fabe9329b3585b7a
Christian Clauss <cclauss@me.com>
2022-05-03 15:22:28 +0200

    from __future__ import absolute_import

--- purl/__init__.py
+++ purl/__init__.py
@@ -1,5 +1,7 @@
-from .url import URL  # noqa
-from .template import expand, Template  # noqa
+from __future__ import absolute_import
+
+from purl.url import URL  # noqa
+from purl.template import expand, Template  # noqa
 
 __version__ = '1.6'
 
